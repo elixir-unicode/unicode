48bf07e2ec052bb17748db094e94e83fd3348651
Moosieus <cameron.duley99@gmail.com>
2023-10-12 20:32:04 -0400

    add validation test helpers

--- test/unicode_validation_test.exs
+++ test/unicode_validation_test.exs
@@ -1,3 +1,6 @@
 defmodule Unicode.Validation.Test do
-
+  # Todo:
+  #   Produce randomized binaries implementations can be run against.
+  #   Implement a way to save interesting or breaking tests.
+  #   Also add a few sanity-check static tests.
 end
