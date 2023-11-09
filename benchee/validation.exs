f97b1b8420b9d5f369cd750bc52e4bd9b17b11d6
Moosieus <cameron.duley99@gmail.com>
2023-11-09 05:05:02 -0500

    improve match context reuse and add benchmarks

---
+++ benchee/validation.exs
@@ -0,0 +1,34 @@
+Code.eval_file("test/support/unicode_validation_helpers.ex")
+alias Unicode.Validation.UTF8.Test.Helpers, as: Helpers
+
+{invalid_I, _valid_I} = Helpers.random_sequences(100)
+{invalid_II, _valid_II} = Helpers.random_sequences(1_000)
+#{invalid_III, _valid_III} = Helpers.random_sequences(100_000)
+{invalid_IV, _valid_IV} = Helpers.random_sequences(1_000_000)
+{invalid_V, valid_V} = Helpers.random_sequences(10_000_000)
+#{invalid_VI, valid_VI} = Helpers.random_sequences(100_000_000)
+
+moby_dick = File.read!("benchee/pg2701.txt")
+
+mb_size = fn x ->
+  "(#{Float.round(byte_size(x) / (1024 ** 2), 2)} MB)"
+end
+
+Benchee.run(
+  %{
+    "replace_invalid" => &Unicode.Validation.UTF8.replace_invalid/1
+  },
+  inputs: %{
+     "100 random sequences #{mb_size.(invalid_I)}" => invalid_I,
+     "1k random sequences #{mb_size.(invalid_II)}" => invalid_II,
+    #"100k sequences" => invalid_III,
+    "1m random sequences #{mb_size.(invalid_IV)}" => invalid_IV,
+    "JSON, Once Invalid (~207KB)" => File.read!("benchee/hll_server_list-single_error.json"),
+    "10m random sequences #{mb_size.(invalid_V)}" => invalid_V,
+    "Moby Dick #{mb_size.(moby_dick)}" => moby_dick,
+    # "100m sequences" => invalid_VI,
+  },
+  time: 2,
+  memory_time: 2,
+  unit_scaling: :smallest
+)
--- lib/unicode/validation/utf8.ex
+++ lib/unicode/validation/utf8.ex
@@ -1,59 +1,59 @@
 defmodule Unicode.Validation.UTF8 do
   @moduledoc false
 
+  import Bitwise
+
   def replace_invalid(bytes, replacement \\ "�") when is_binary(bytes) and is_binary(replacement) do
     do_replace(bytes, replacement, <<>>)
   end
 
   # ASCII (for better average speed)
-  defp do_replace(<<ascii::8, n_lead::2, rest::bits>>, rep, acc) when ascii in 0..127 and n_lead != 0b10 do
-    do_replace(<<n_lead::2, rest::bits>>, rep, <<acc::bits, ascii::8>>)
+  defp do_replace(<<ascii::8, next::8, _::bytes>> = rest, rep, acc) when ascii in 0..127 and (next >>> 6) !== 0b10 do
+    <<_::8, rest::bytes>> = rest
+    do_replace(rest, rep, acc <> <<ascii::8>>)
   end
 
   # UTF-8 (valid)
-  defp do_replace(<<grapheme::utf8, rest::bits>>, rep, acc) do
-    do_replace(rest, rep, <<acc::bits, grapheme::utf8>>)
+  defp do_replace(<<grapheme::utf8, rest::bytes>>, rep, acc) do
+    do_replace(rest, rep, acc <> <<grapheme::utf8>>)
   end
 
   # 2/3 truncated sequence
 
   defp do_replace(<<0b1110::4, i::4, 0b10::2, ii::6>>, rep, acc) do
-    <<tcp::10>> = <<i::4, ii::6>>
-    <<acc::bits, ii_of_iii(tcp, rep)::bits>>
+    acc <> ii_of_iii(<<i::4, ii::6>>, rep)
   end
 
-  defp do_replace(<<0b1110::4, i::4, 0b10::2, ii::6, n_lead::2, rest::bits>>, rep, acc) when n_lead != 0b10 do
-    <<tcp::10>> = <<i::4, ii::6>>
-    do_replace(<<n_lead::2, rest::bits>>, rep, <<acc::bits, ii_of_iii(tcp, rep)::bits>>)
+  defp do_replace(<<0b1110::4, i::4, 0b10::2, ii::6, next::8, _::bytes>> = rest, rep, acc) when (next >>> 6) !== 0b10 do
+    <<_::16, rest::bytes>> = rest
+    do_replace(rest, rep, acc <> ii_of_iii(<<i::4, ii::6>>, rep))
   end
 
   # 2/4
 
   defp do_replace(<<0b11110::5, i::3, 0b10::2, ii::6>>, rep, acc) do
-    <<tcp::10>> = <<i::4, ii::6>>
-    <<acc::bits, ii_of_iiii(tcp, rep)::bits>>
+    acc <> ii_of_iv(<<i::3, ii::6>>, rep)
   end
 
-  defp do_replace(<<0b11110::5, i::3, 0b10::2, ii::6, n_lead::2, rest::bits>>, rep, acc) when n_lead != 0b10 do
-    <<tcp::10>> = <<i::4, ii::6>>
-    do_replace(<<n_lead::2, rest::bits>>, rep, <<acc::bits, ii_of_iiii(tcp, rep)::bits>>)
+  defp do_replace(<<0b11110::5, i::3, 0b10::2, ii::6, next::8, _::bytes>> = rest, rep, acc) when (next >>> 6) !== 0b10 do
+    <<_::16, rest::bytes>> = rest
+    do_replace(rest, rep, acc <> ii_of_iv(<<i::3, ii::6>>, rep))
   end
 
   # 3/4
 
   defp do_replace(<<0b11110::5, i::3, 0b10::2, ii::6, 0b10::2, iii::6>>, rep, acc) do
-    <<tcp::15>> = <<i::3, ii::6, iii::6>>
-    <<acc::bits, iii_of__iiii(tcp, rep)::bits>>
+    acc <> iii_of_iv(<<i::3, ii::6, iii::6>>, rep)
   end
 
-  defp do_replace(<<0b11110::5, i::3, 0b10::2, ii::6, 0b10::2, iii::6, n_lead::2, rest::bits>>, rep, acc) when n_lead != 0b10 do
-    <<tcp::15>> = <<i::3, ii::6, iii::6>>
-    do_replace(<<n_lead::2, rest::bits>>, rep, <<acc::bits, iii_of__iiii(tcp, rep)::bits>>)
+  defp do_replace(<<0b11110::5, i::3, 0b10::2, ii::6, 0b10::2, iii::6, next::8, _::bytes>> = rest, rep, acc) when (next >>> 6) !== 0b10 do
+    <<_::24, rest::bytes>> = rest
+    do_replace(rest, rep, acc <> iii_of_iv(<<i::3, ii::6, iii::6>>, rep))
   end
 
   # Everything else
 
-  defp do_replace(<<_, rest::bits>>, rep, acc), do: do_replace(rest, rep, <<acc::bits, rep::bits>>)
+  defp do_replace(<<_, rest::bytes>>, rep, acc), do: do_replace(rest, rep, acc <> rep)
 
   # Final
 
@@ -61,13 +61,13 @@ defmodule Unicode.Validation.UTF8 do
 
   # bounds-checking truncated code points for overlong encodings
 
-  defp ii_of_iii(tcp, rep) when tcp >= 32 and tcp <= 863, do: rep
-  defp ii_of_iii(tcp, rep) when tcp >= 896 and tcp <= 1023, do: rep
+  defp ii_of_iii(<<tcp::10>>, rep) when tcp >= 32 and tcp <= 863, do: rep
+  defp ii_of_iii(<<tcp::10>>, rep) when tcp >= 896 and tcp <= 1023, do: rep
   defp ii_of_iii(_, rep), do: rep <> rep
 
-  defp ii_of_iiii(tcp, rep) when tcp >= 16 and tcp <= 271, do: rep
-  defp ii_of_iiii(_, rep), do: rep <> rep
+  defp ii_of_iv(<<tcp::9>>, rep) when tcp >= 16 and tcp <= 271, do: rep
+  defp ii_of_iv(_, rep), do: rep <> rep
 
-  defp iii_of__iiii(tcp, rep) when tcp >= 1024 and tcp <= 17407, do: rep
-  defp iii_of__iiii(_, rep), do: rep <> rep <> rep
+  defp iii_of_iv(<<tcp::15>>, rep) when tcp >= 1024 and tcp <= 17407, do: rep
+  defp iii_of_iv(_, rep), do: rep <> rep <> rep
 end
