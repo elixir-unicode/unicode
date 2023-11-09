0f37466fa354577f89c884c7162690a1a310d8be
Moosieus <cameron.duley99@gmail.com>
2023-11-09 17:48:57 -0500

    compare `in/2` performance

--- benchee/validation.exs
+++ benchee/validation.exs
@@ -3,10 +3,10 @@ alias Unicode.Validation.UTF8.Test.Helpers, as: Helpers
 
 {invalid_I, _valid_I} = Helpers.random_sequences(100)
 {invalid_II, _valid_II} = Helpers.random_sequences(1_000)
-#{invalid_III, _valid_III} = Helpers.random_sequences(100_000)
+{invalid_III, _valid_III} = Helpers.random_sequences(100_000)
 {invalid_IV, _valid_IV} = Helpers.random_sequences(1_000_000)
-{invalid_V, valid_V} = Helpers.random_sequences(10_000_000)
-#{invalid_VI, valid_VI} = Helpers.random_sequences(100_000_000)
+{invalid_V, _valid_V} = Helpers.random_sequences(10_000_000)
+{invalid_VI, _valid_VI} = Helpers.random_sequences(100_000_000)
 
 moby_dick = File.read!("benchee/pg2701.txt")
 
@@ -16,19 +16,20 @@ end
 
 Benchee.run(
   %{
-    "replace_invalid" => &Unicode.Validation.UTF8.replace_invalid/1
+    "replace_invalid" => &Unicode.Validation.UTF8.replace_invalid/1,
+    "replace_invalid_in" => &Unicode.Validation.UTF8_IN.replace_invalid/1,
   },
   inputs: %{
-     "100 random sequences #{mb_size.(invalid_I)}" => invalid_I,
-     "1k random sequences #{mb_size.(invalid_II)}" => invalid_II,
-    #"100k sequences" => invalid_III,
+    "100 random sequences #{mb_size.(invalid_I)}" => invalid_I,
+    "1k random sequences #{mb_size.(invalid_II)}" => invalid_II,
+    "100k sequences" => invalid_III,
     "1m random sequences #{mb_size.(invalid_IV)}" => invalid_IV,
     "JSON, Once Invalid (~207KB)" => File.read!("benchee/hll_server_list-single_error.json"),
     "10m random sequences #{mb_size.(invalid_V)}" => invalid_V,
     "Moby Dick #{mb_size.(moby_dick)}" => moby_dick,
-    # "100m sequences" => invalid_VI,
+    "100m sequences" => invalid_VI,
   },
-  time: 2,
-  memory_time: 2,
+  time: 10,
+  memory_time: 5,
   unit_scaling: :smallest
 )
---
+++ lib/unicode/validation/utf8_in.ex
@@ -0,0 +1,77 @@
+defmodule Unicode.Validation.UTF8_IN do
+  @moduledoc false
+
+  defguardp is_truncated_ii_of_iii(i, ii) when
+    Bitwise.bor(Bitwise.bsl(i, 6), ii) in 32..863
+    or
+    Bitwise.bor(Bitwise.bsl(i, 6), ii) in 896..1023
+
+  defguardp is_truncated_ii_of_iv(i, ii) when
+    Bitwise.bor(Bitwise.bsl(i, 6), ii) in 16..271
+
+  defguardp is_truncated_iii_of_iv(i, ii, iii) when
+    Bitwise.bor(Bitwise.bor(Bitwise.bsl(i, 12), Bitwise.bsl(ii, 6)), iii) in 1024..17407
+
+  defguardp is_ascii(codepoint) when codepoint in 0..127
+
+  defguardp is_next_sequence(next) when Bitwise.bsr(next, 6) !== 0b10
+
+  def replace_invalid(bytes, replacement \\ "�")
+     when is_binary(bytes) and is_binary(replacement) do
+    do_replace(bytes, replacement, <<>>)
+  end
+
+  # ASCII (for better average speed)
+  defp do_replace(<<ascii::8, next::8, _::bytes>> = rest, rep, acc)
+     when is_ascii(ascii) and is_next_sequence(next) do
+    <<_::8, rest::bytes>> = rest
+    do_replace(rest, rep, acc <> <<ascii::8>>)
+  end
+
+  # UTF-8 (valid)
+  defp do_replace(<<grapheme::utf8, rest::bytes>>, rep, acc) do
+    do_replace(rest, rep, acc <> <<grapheme::utf8>>)
+  end
+
+  # 2/3 truncated sequence
+  defp do_replace(<<0b1110::4, i::4, 0b10::2, ii::6>>, rep, acc)
+     when is_truncated_ii_of_iii(i, ii) do
+    acc <> rep
+  end
+
+  defp do_replace(<<0b1110::4, i::4, 0b10::2, ii::6, next::8, _::bytes>> = rest, rep, acc)
+     when is_truncated_ii_of_iii(i, ii) and is_next_sequence(next) do
+    <<_::16, rest::bytes>> = rest
+    do_replace(rest, rep, acc <> rep)
+  end
+
+  # 2/4
+  defp do_replace(<<0b11110::5, i::3, 0b10::2, ii::6>>, rep, acc)
+     when is_truncated_ii_of_iv(i, ii) do
+    acc <> rep
+  end
+
+  defp do_replace(<<0b11110::5, i::3, 0b10::2, ii::6, next::8, _::bytes>> = rest, rep, acc)
+     when is_truncated_ii_of_iv(i, ii) and is_next_sequence(next) do
+    <<_::16, rest::bytes>> = rest
+    do_replace(rest, rep, acc <> rep)
+  end
+
+  # 3/4
+  defp do_replace(<<0b11110::5, i::3, 0b10::2, ii::6, 0b10::2, iii::6>>, rep, acc)
+     when is_truncated_iii_of_iv(i, ii, iii) do
+    acc <> rep
+  end
+
+  defp do_replace(<<0b11110::5, i::3, 0b10::2, ii::6, 0b10::2, iii::6, next::8, _::bytes>> = rest, rep, acc)
+     when is_truncated_iii_of_iv(i, ii, iii) and is_next_sequence(next) do
+    <<_::24, rest::bytes>> = rest
+    do_replace(rest, rep, acc <> rep)
+  end
+
+  # Everything else
+  defp do_replace(<<_, rest::bytes>>, rep, acc), do: do_replace(rest, rep, acc <> rep)
+
+  # Final
+  defp do_replace(<<>>, _, acc), do: acc
+end
